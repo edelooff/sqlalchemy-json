b3eb62bb3753033563d44f90b55442177115bff0
Elmer de Looff <elmer.delooff@gmail.com>
2017-09-09 21:18:08 +0100

    Updated setuptools script, lowered version number to reflect beta development stage.

--- setup.py
+++ setup.py
@@ -1,22 +1,34 @@
-from setuptools import setup, find_packages
+import os
+from setuptools import setup
 
-if __name__ == "__main__":
-    setup(
-        name='sqlalchemy-json',
-        version="1.0.0",
-        description="A full-featured JSON type with mutation tracking for SQLAlchemy",
-        author="Elmer de Looff",
-        author_email="elmer.delooff@gmail.com",
-        url="https://github.com/edelooff/sqlalchemy-json",
-        packages=find_packages(),
-        install_requires=[
-            'sqlalchemy>=0.7',
-            'sqlalchemy_utils',
-        ],
-        zip_safe=False,
-        classifiers=[
-            'License :: OSI Approved :: BSD License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-        ],
-    )
+
+def contents(filename):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, filename)) as fp:
+        return fp.read()
+
+
+setup(
+    name='sqlalchemy-json',
+    version='0.2.1',
+    author='Elmer de Looff',
+    author_email='elmer.delooff@gmail.com',
+    description='JSON type with nested change tracking for SQLAlchemy',
+    long_description=contents('README.rst'),
+    keywords='sqlalchemy json mutable',
+    license='BSD',
+    url='https://github.com/edelooff/sqlalchemy-json',
+    packages=['sqlalchemy_json'],
+    install_requires=[
+        'six',
+        'sqlalchemy>=0.7',
+        'sqlalchemy_utils'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database'])
