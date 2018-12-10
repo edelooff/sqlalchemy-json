aff71af4934f1448acb1073ab019fa6528bb04c4
Elmer de Looff <elmer.delooff@gmail.com>
2018-12-10 11:22:08 +0100

    Patch version increase for release.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def contents(filename):
 
 setup(
     name='sqlalchemy-json',
-    version='0.2.2',
+    version='0.2.3',
     author='Elmer de Looff',
     author_email='elmer.delooff@gmail.com',
     description='JSON type with nested change tracking for SQLAlchemy',
